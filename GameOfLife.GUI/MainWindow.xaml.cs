625480fec3be8550dbb880a46418bf17aad6120a
Jonathan Åkerblom <jonathan.akerblom@live.se>
2016-12-17 15:10:54 +0100

    Added untracked files

--- GameOfLife.GUI/MainWindow.xaml.cs
+++ GameOfLife.GUI/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameOfLife.Models;
 
 namespace GameOfLife.GUI
 {
@@ -20,9 +21,51 @@ namespace GameOfLife.GUI
   /// </summary>
   public partial class MainWindow : Window
   {
+    GUISettings _gs;
+    Simulator _sim;
+    const int GridSize = 20;
+
     public MainWindow()
     {
       InitializeComponent();
+      //Window_Loaded();
+      _gs = new GUISettings() { Width = 20, Height = 20 };
+      _sim = new Simulator(_gs);
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      for (int i = _gs.LowerBoundHorizontal; i <= _gs.Width; i++)
+      {
+        simGrid.ColumnDefinitions.Add(
+                      new ColumnDefinition());
+      }
+      for (int i = _gs.LowerBoundVertical; i <= _gs.Height; i++)
+      {
+        simGrid.RowDefinitions.Add(
+                      new RowDefinition());
+      }
+
+      for (int i = _gs.LowerBoundHorizontal; i <= _gs.Width; i++)
+      {
+        for (int j = _gs.LowerBoundVertical; j <= _gs.Height; j++)
+        {
+          Rectangle rect = new Rectangle();
+          Grid.SetColumn(rect, j-1);
+          Grid.SetRow(rect, i-1);
+          rect.DataContext = _sim.GetCell(i, j);
+          //rect.SetBinding(OpacityProperty);
+          rect.Fill = Brushes.AliceBlue;
+          simGrid.Children.Add(rect);
+        }
+      }
+
+      simGrid.ShowGridLines = true;
+    }
+
+    private void Window_ContentRendered(object sender, RoutedEventArgs e)
+    {
+
     }
 
     private void button_Click(object sender, RoutedEventArgs e)
